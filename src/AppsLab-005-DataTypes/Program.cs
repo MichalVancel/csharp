1545fbc78dd0c8dd975111ce2c3c74535db25cc6
Michal Vancel <vanmichal77@gmail.com>
2025-09-24 09:26:28 +0200

    Vypracoval som ulohu 5

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int mojeCislo = 1234;
+string mojeSlovo = "Ahoj kamarádi";
+bool mojaBoolhodnota = true;
+float myFloat = 2.5f;
+var noveCislo = 255;
+DateTime dnesnyDatum = DateTime.Now; 
+int sucet = mojeCislo + 10;
+Console.WriteLine(mojeCislo.ToString("00000"));   
+Console.WriteLine(mojeSlovo);
+Console.WriteLine($"Súčet je: {sucet}");   
+Console.WriteLine($"Dnes je: {dnesnyDatum.ToString ("yyyy-MM-dd")}");
+Console.WriteLine($"Bool hodnota je: {mojaBoolhodnota}");
+Console.WriteLine(myFloat); 
+Console.WriteLine(noveCislo);
+
+
+
+
+
